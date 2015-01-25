e8ea732713d58399119e6ed8537747d3ed0ba51c
Leon <a.li.devtty@gmail.com>
2015-01-25 17:32:55 +0800

    use of goroutine facilitates processing speed

--- main.go
+++ main.go
@@ -11,9 +11,13 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
+const SIMUL_GOR = 10
+
 var inplace bool
+var wg sync.WaitGroup
 
 func init() {
 	flag.Usage = func() {
@@ -35,56 +39,78 @@ func main() {
 	}
 
 	path := flag.Arg(0)
+	paths := strings.Split(path, ":::")
+	tagss := make([][]*nodefinder.Elem, 0, len(paths))
+	for _, p := range paths {
+		tagss = append(tagss, nodefinder.NewPath(p))
+	}
+
+	var simul_gor int
+	if inplace {
+		simul_gor = SIMUL_GOR
+	} else {
+		simul_gor = 1
+	}
+	wait_chan := make(chan int, simul_gor)
+
 	for _, f := range flag.Args()[1:] {
-		func() {
-		found := 0
-		fh, err := os.Open(f)
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		defer fh.Close()
-		root, err := html.Parse(fh)
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		for _, p := range strings.Split(path, ":::") {
-			tags := nodefinder.NewPath(p)
-			nodes := nodefinder.FindByNode(tags, root)
+
+		wait_chan <- 1
+		wg.Add(1)
+
+		go func(filename string) {
+			defer func() {
+				<-wait_chan
+				wg.Done()
+			}()
+			found := 0
+			fh, err := os.Open(filename)
 			if err != nil {
 				log.Print(err)
 				return
 			}
-			for _, n := range nodes {
-				if n.Parent != nil {
-					n.Parent.RemoveChild(n)
-					found++
-				} else {
-					log.Print(errors.New("Found node has no parent, can not be removed"))
+			defer fh.Close()
+			root, err := html.Parse(fh)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			for _, tags := range tagss {
+				nodes := nodefinder.FindByNode(tags, root)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+				for _, n := range nodes {
+					if n.Parent != nil {
+						n.Parent.RemoveChild(n)
+						found++
+					} else {
+						log.Print(errors.New("Found node has no parent, can not be removed"))
+					}
 				}
 			}
-		}
-		if !inplace {
-			html.Render(os.Stdout, root)
-		} else if found > 0 {
-			tempfile, err := ioutil.TempFile(filepath.Dir(f), "htmlcleaner")
-			if err != nil {
-				log.Print(err)
-				return
+			if !inplace {
+				html.Render(os.Stdout, root)
+			} else if found > 0 {
+				tempfile, err := ioutil.TempFile(filepath.Dir(filename), "htmlcleaner")
+				if err != nil {
+					log.Print(err)
+					return
+				}
+				err = html.Render(tempfile, root)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+				tempfile.Close()
+				err = os.Rename(tempfile.Name(), filename)
+				if err != nil {
+					log.Print(err)
+					return
+				}
 			}
-			err = html.Render(tempfile, root)
-			if err != nil {
-				log.Print(err)
-				return
-			}
-			tempfile.Close()
-			err = os.Rename(tempfile.Name(), f)
-			if err != nil {
-				log.Print(err)
-				return
-			}
-		}
-	}()
+		}(f)
 	}
+	wg.Wait()
 }
